af30a1b0cda3c3830d4161c7d0a0fdd01c008f38
Niko <yejinms@naver.com>
2025-05-18 20:50:18 +0900

    iOS / WatchOS 앱 분리

---
+++ AvoJump IOS AppTests/AvoJump_IOS_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AvoJump_IOS_AppTests.swift
+//  AvoJump IOS AppTests
+//
+//  Created by Niko on 5/18/25.
+//
+
+import Testing
+@testable import AvoJump_IOS_App
+
+struct AvoJump_IOS_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
